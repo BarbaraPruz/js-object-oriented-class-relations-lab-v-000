65e18ba9e01f69a3c711a75bb6689c8eab8878c4
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-30 21:40:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
-let store = {drivers: [], passengers: []}
+let store = {drivers: [], passengers: [], trips: []}
 
 let driverId = 0;
 let passengerId = 0;
+let tripId = 0;
 
 class Driver {
   constructor (name) {
@@ -17,5 +18,14 @@ class Passenger {
     this.name = name;
     this.id = ++passengerId;
     store.passengers.push(this)
+  }
+}
+
+class Trip {
+  constructor (driver, passenger) {
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    store.trips.push(this);
   }
 }
