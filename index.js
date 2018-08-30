d2f0d2860f5d5c65a177dbab5a64804fdc504117
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-30 21:37:56 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+
+let store = {drivers: [], passengers: []}
+
+let driverId = 0;
+let passengerId = 0;
+
+class Driver {
+  constructor (name) {
+    this.name = name;
+    this.id = ++driverId;
+    store.drivers.push(this)
+  }
+}
+
+class Passenger {
+  constructor (name) {
+    this.name = name;
+    this.id = ++passengerId;
+    store.passengers.push(this)
+  }
+}
